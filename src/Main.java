f135c6ad4182b82100e385903bd1f13c989f9cab
ViktorKovalyshyn <komediantpet@gmail.com>
2024-04-18 15:16:16 +0300

    to lab1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+Виконав студент групи СІ-22
+Ковалишин Віктор Петрович
+*/
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
